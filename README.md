4f5a1cc84d4b018650391c13c7468ae59fb04d6a
arunpaandyan <75234443+arunpaandyan@users.noreply.github.com>
2023-03-22 08:32:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-crud-op
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d2sfsb)
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState,useEffect } from 'react';
+import Header from './Header';
+import AddItem from './AddItem';
+import Content from './Content';
+import Footer from './Footer';
+import SearchItem from './SearchItem';
+
+export default function App() {
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('shoppiglist')) || []
+  );
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+
+  useEffect(()=>{
+    localStorage.setItem('shoppiglist', JSON.stringify(items));
+  },[items])
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+
+    addItem(newItem);
+    setNewItem('');
+    console.log('Submitted');
+  };
+
+  return (
+    <div className="App">
+      <Header title="Groceries List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
+    </div>
+  );
+}
